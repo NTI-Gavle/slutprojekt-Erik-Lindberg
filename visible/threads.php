7f0c710b11ad3c4db7ff82d46a1e158abb99f5e1
Erik-Lindberg <erik.lindberg@elev.ga.ntig.se>
2026-03-23 14:55:26 +0100

    I have made the files exist and have made a small amount of css

---
+++ visible/threads.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../public/css/kwitter.css">
+</head>
+<body>
+    <aside>
+        <a href="threads.php"><div id="logo">Logo</div></a>
+        <a href="threads.php"><div class="sidbtn">THREADS</div></a>
+        <a href="search.php"><div class="sidbtn">SEARCH</div></a>
+        <a href="follow.php"><div class="sidbtn">FOLLOW</div></a>
+        <a href="profile.php"><div class="sidbtn">PROFILE</div></a>
+        <a href="settings.php"><div class="sidbtn">SETTINGS</div></a>
+    </aside>
+</body>
+</html>
