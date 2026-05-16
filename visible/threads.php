45c745994f1e513440386176c43df059f281920b
Erik-Lindberg <erik.lindberg@elev.ga.ntig.se>
2026-05-16 17:43:07 +0200

    Added Search function Added poster username to posts Removed Like button and reply button Fixed issue of certain classes having been written with id Changed how Posts look

--- visible/threads.php
+++ visible/threads.php
@@ -9,7 +9,7 @@ $POS = $_SESSION["POS"];
 if(!isset($_SESSION["User"])){
     header("Location: Login.php"); 
   }
-  $sql = "SELECT p.PosterID, p.PostContent, p.ReplyID, p.ReplyCount, p.LikeCount AS PostLikeCount, r.ReplyContent, r.LikeCount AS ReplyLikeCount, r.PostID FROM posts p LEFT JOIN replies r ON p.ID = r.PostID WHERE p.ID=? ORDER BY p.ID, r.PostID";
+  $sql = "SELECT p.PosterID, p.PostContent, p.ReplyID, p.ReplyCount, p.LikeCount, r.ReplyContent, r.LikeCount AS ReplyLikeCount, r.PostID, u.Username FROM posts p LEFT JOIN replies r ON p.ID = r.PostID LEFT JOIN users u ON p.PosterID = u.UID WHERE p.ID=? ORDER BY p.ID, r.PostID, u.UID";
     $stmt = $dbconn->prepare($sql);
     $stmt->execute([$POS]);
     $p = $stmt->fetch(PDO::FETCH_ASSOC);
@@ -23,21 +23,21 @@ if(!isset($_SESSION["User"])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../public/css/kwitter.css">
+    <link rel="stylesheet" href="../public/css/kwitter.css?v=1">
+    <script src="../public/js/app.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </head>
 <body>
 <div class="row">
-  <div class="col"><?php include "aside.php";?></div>
+  <div class="col Sidebar"><?php include "aside.php";?></div>
   <div class="col Threads">
     <?php if ($p): ?>
-      <div id="Thread-container">
-        <div id="ThreadContent">
+      <div class="ThreadContainer p-3 rounded">
+        <div class="ThreadContent">
           <p class="text-light"><?= htmlspecialchars($p["PostContent"]) ?></p>
         </div>
-        <button type="button" class="likebtn">Like</button>
-        <button type="button" class="replybtn">Reply</button>
+        <small class="text-secondary">Poster: <?=htmlspecialchars($p["Username"])?></small>
       </div>
     <?php else: ?>
       <p  class="text-light">No posts found.</p>
